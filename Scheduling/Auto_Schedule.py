f961dd23b29875be49d14a672123ad932562b3a6
mohsenusr <msalehi.usr@gmail.com>
2021-11-05 23:28:55 +0100

    Scheduling

---
+++ Scheduling/Auto_Schedule.py
@@ -0,0 +1,14 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+sched = BlockingScheduler()
+
+@sched.scheduled_job('interval', seconds=10)
+def timed_job():
+    print('This job is run every 10 seconds.')
+
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=10)
+def scheduled_job():
+    print('This job is run every weekday at 10am.')
+
+sched.configure(options_from_ini_file)
+sched.start()
